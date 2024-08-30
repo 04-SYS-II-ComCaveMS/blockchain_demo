2a7bae0cf73df0d4e54c36778b9ff4beae0aefbf
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-08-30 12:10:59 +0200

    03/ Blockchain

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block{
 
     constructor(timestamp,lastHash,hash,data){
@@ -15,9 +17,25 @@ class Block{
         Hash:       ${this.hash}
         Data:       ${this.data}`
     }
+
     static genesis(){ // 1. Block der chain
         return new this("Genesis time","-----","xABc1234",["data1","data2,data3"]); // call constructor
-
     }
+
+    static mineBlock(lastBlock,ownData){ // weitere Blocks in der Chain
+
+        const timestamp = Date.now(); // Zeit im ms seit 01.01.1970 | UNIX
+        const lastHash  = lastBlock.hash;
+        const hash      = Block.hash(timestamp, lastHash, ownData);
+
+        return new this(timestamp,lastHash,hash,ownData);
+
+  }
+
+  static hash(timestamp,lastHash,data){
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  }
+
 }
+
 module.exports = Block; // Export als Modul
