5d62fb8dc3e5ab50136c63d3ecab89ff479146b4
Marco Grochowiak <marco.grochowiak@outlook.com>
2024-08-30 12:23:53 +0200

    04/ update

--- dev-test.js
+++ dev-test.js
@@ -1,4 +1,5 @@
 // Import
+const Blockchain = require("./blockchain");
 const Block = require("./block");
 
 // /***  Test der Block-Funktionalität  */
@@ -7,5 +8,15 @@ const Block = require("./block");
 
 // console.log(Block.genesis().toString());
 
-const testBlock = Block.mineBlock(Block.genesis(),"testDaten");
-console.log(testBlock.toString());
+// const testBlock = Block.mineBlock(Block.genesis(),"testDaten");
+// console.log(testBlock.toString());
+
+const chain = new Blockchain();
+chain.addBlock("1034,98€ von Sven");
+console.log(chain);
+
+chain.addBlock("890,01€ von Saba");
+console.log(chain);
+
+chain.addBlock("20,00€ von Saba, 75,00€ von Soraya");
+console.log(chain);
